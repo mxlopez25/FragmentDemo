615656f3313abe0f274c49090d8782f88d85aae2
Maximo Lopez <maximo@craftware.co>
2018-10-26 13:31:32 -0400

    FormFragment set up

--- app/src/main/java/com/example/maximolopez/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/maximolopez/fragmentdemo/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.maximolopez.fragmentdemo;
 
 import android.os.Bundle;
+import android.os.FileObserver;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.maximolopez.fragmentdemo.fragments.FormFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
+    private FormFragment formFragment = new FormFragment();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    FormFragment fForm = (FormFragment) fragmentManager.findFragmentByTag(FormFragment.TAG);
+                    if(fForm != null && !fForm.isVisible())
+                    {
+                        switchFragment(formFragment);
+                    } else {
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.fl_fragments, formFragment, FormFragment.TAG)
+                                .commit();
+                        return true;
+                    }
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -39,6 +60,20 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.fl_fragments, new FormFragment(), FormFragment.TAG).commit();
+
     }
 
+    public boolean switchFragment(Fragment fragment)
+    {
+        fragmentManager.beginTransaction()
+                .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                .show(fragment)
+                .commit();
+        return true;
+    }
 }
